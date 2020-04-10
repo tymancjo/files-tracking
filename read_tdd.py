e2d4f8c4cfcb3d91646b44980848188b9f6ba163
TymancjO <tomasz.tomanek@gmail.com>
2020-04-10 11:52:08 +0200

    1st workingversion - still no OS independent

--- read_tdd.py
+++ read_tdd.py
@@ -16,19 +16,51 @@ def read_files(directory, files_ext):
     
     return list_of_files
 
+def data_process(list_of_files):
+    data_from_files = []
 
-data_from_files = []
+    for file_ in list_of_files:
+        data_from_single_file = []
+        with open(file_ ,encoding='ISO-8859-1') as f:
+            for line in f:
+                data_from_single_file.append(line)
+        data_from_files.extend(data_from_single_file[22:])
 
-for file_ in read_files("data","TDD"):
-    data_from_single_file = []
-    with open(file_ ,encoding='ISO-8859-1') as f:
-        for line in f:
-            data_from_single_file.append(line)
-    data_from_files.extend(data_from_single_file[22:])
+    final_data = []
+    final_marks = []
 
-print(data_from_files[0])
-print(data_from_files[1])
+    for ix,data_line in enumerate(data_from_files):
+        the_line = data_line.split("\t")
+        clean_line = []
+        # lets clean data in this line
+        # starting from 3th (1st is time, 2nd is min, 3th is first max) 
+        # andstepping by 2 - taking themax only
+        for element in the_line[2::2]:
+            # skipping 2 first as those are date and time
+            element_value = float(element)
 
+            # zeroing the big values 
+            if element_value > 500:
+                element_value = "X"
+            
+            clean_line.append(element_value)
 
+        time_stamp = the_line[0].split(" ")[1][:-4]
+        max_position = clean_line.index("X")
+        # collecting final data
+        final_marks.append(time_stamp)
+        final_data.append(clean_line[:max_position])
+
+    final_data = np.array(final_data)
+    return final_marks, final_data
+
+def plot_data(input_data):
+    final_marks,final_data = input_data
+    plt.clf()
+    plt.plot(final_marks,final_data)
+    plt.xticks( final_marks[::20], rotation='vertical')
+    plt.ion()
+    plt.show()
 
     
+plot_data(data_process(read_files("data","TDD")))
--- wd.py
+++ wd.py
@@ -1,9 +1,77 @@
 # following
 # http://thepythoncorner.com/dev/how-to-create-a-watchdog-in-python-to-look-for-filesystem-changes/
 
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
 import time, sys, os
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
+
+matplotlib.use("TkAgg")
+
+def read_files(directory, files_ext):
+    
+    files_path = str(directory)+'/*.'+str(files_ext)
+    
+    print(f"reading from: {files_path}")
+
+    list_of_files = glob.glob(files_path)
+    list_of_files.sort()
+    
+    return list_of_files
+
+def data_process(list_of_files):
+    data_from_files = []
+
+    for file_ in list_of_files:
+        data_from_single_file = []
+        with open(file_ ,encoding='ISO-8859-1') as f:
+            for line in f:
+                data_from_single_file.append(line)
+        data_from_files.extend(data_from_single_file[22:])
+
+    final_data = []
+    final_marks = []
+
+    for ix,data_line in enumerate(data_from_files):
+        the_line = data_line.split("\t")
+        clean_line = []
+        # lets clean data in this line
+        # starting from 3th (1st is time, 2nd is min, 3th is first max) 
+        # andstepping by 2 - taking themax only
+        for element in the_line[2::2]:
+            # skipping 2 first as those are date and time
+            element_value = float(element)
+
+            # zeroing the big values 
+            if element_value > 500:
+                element_value = "X"
+            
+            clean_line.append(element_value)
+
+        time_stamp = the_line[0].split(" ")[1][:-4]
+        max_position = clean_line.index("X")
+        # collecting final data
+        final_marks.append(time_stamp)
+        final_data.append(clean_line[:max_position])
+
+    final_data = np.array(final_data)
+    return final_marks, final_data
+
+def plot_data(input_data):
+    final_marks,final_data = input_data
+    plt.clf()
+    plt.plot(final_marks,final_data)
+    plt.xticks( final_marks[::20], rotation='vertical')
+    # plt.ion()
+    # plt.show()
+    plt.draw()
+    plt.pause(0.01)
+    
+
 
 def on_created(event):
     print(f"hey, {event.src_path} has been created!")
@@ -15,7 +83,9 @@ def on_deleted(event):
     print(f"what the f**k! Someone deleted {event.src_path}!")
 
 def on_modified(event):
-    print(f"hey buddy, {event.src_path} has been modified")
+    plot_data(data_process(read_files("data","TDD")))
+
+    # print(f"hey buddy, {event.src_path} has been modified")
 
 def on_moved(event):
     print(f"ok ok ok, someone moved {event.src_path} to {event.dest_path}")
